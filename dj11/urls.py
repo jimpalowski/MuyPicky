c32adf8f15c1d226c1c0ef3bf60f9552e0594176
Jim Palowski <palowskijim@gmail.com>
2018-02-05 20:33:34 -0800

    Updating

--- dj11/urls.py
+++ dj11/urls.py
@@ -18,17 +18,18 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 from restraunts.views import (
+  restraunt_createview,
 	RestrauntListView,
 	restraunt_listview,
 	RestrauntDetailView,
-  restraunt_createview,
+  RestrauntCreateView,
 	)
 
 urlpatterns = [
    url('^admin/', admin.site.urls),
    url('^$', TemplateView.as_view(template_name='home.html')),
    url('^restraunts/$', RestrauntListView.as_view()),
-   url('^restraunts/create/$', restraunt_createview),
+   url('^restraunts/create/$', RestrauntCreateView.as_view()),
    url('^restraunts/(?P<slug>[\w-]+)/$', RestrauntDetailView.as_view()),
    #url('^restraunts/asian/$', AsianFusionRestrauntListView.as_view()),
    url('^about/$', TemplateView.as_view(template_name='about.html')),
--- restraunts/views.py
+++ restraunts/views.py
@@ -1,23 +1,33 @@
 from django.db.models import Q
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
 
 
-from.forms import RestrauntCreateForm
+from.forms import RestrauntCreateForm, RestrauntLocationCreateForm
 from .models import RestrauntLocation
 
 
 def restraunt_createview(request):
-	if request.method == "GET":
-		print("get data")
-	if request.method == "POST":
-		print("post data")
-		print(request.POST)	
+	form = RestrauntLocationCreateForm(request.POST or None)
+	errors = None
+	if form.is_valid():
+		if request.user.is_authenticated():
+		 	instance = form.save(commit=False)
+
+
+		 	instance.owner = request.user
+		 	form.save() 
+		 	return HttpResponseRedirect("/restraunts/")
+		else:
+			return HttpResponseRedirect("/login/")
+	if form.errors: 
+		errors = form.errors
+
 	template_name = 'restraunts/form.html'
-	context = {}
+	context = {"form": form , "errors": errors}
 	return render(request, template_name, context)
 
 
@@ -60,13 +70,25 @@ class RestrauntListView(ListView):
 
 
 class RestrauntDetailView(DetailView):
-	queryset = RestrauntLocation.objects.all()#filter(category__iexact='asian')
+	queryset = RestrauntLocation.objects.all()
 
-	'''def get_object(self, *args, **kwargs):
-		rest_id = self.kwargs.get('rest_id')
-		obj = get_object_or_404(RestrauntLocation, id=rest_id)
-		return obj 
+class RestrauntCreateView(CreateView):
+	form_class = RestrauntLocationCreateForm
+	template_name = 'restraunts/form.html'
+	success_url = "/restraunts/"
 
-'''
+
+	def form_valid(self, form):
+		instance = form.save(commit=False)
+		instance.owner = self.request.user
+		return super(RestrauntCreateView, self).form_valid(form)
+
+
+
+
+
+
+
+
 
 
